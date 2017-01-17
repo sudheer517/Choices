be03a8f6968122dde85d45277482bbe9dea32bfa
Rakesh Rohan A <rakeshrohan.aitipamula@quantium.com>
2017-01-17 18:22:43 +0530

    (fix): case management form1

--- src/MyChoices.Portal/src/app/pages/caseManagement/caseManagement.component.ts
+++ src/MyChoices.Portal/src/app/pages/caseManagement/caseManagement.component.ts
@@ -1,4 +1,5 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'case-management',
@@ -7,7 +8,38 @@ import {Component, ViewEncapsulation} from '@angular/core';
   template: require('./caseManagement.html')
 })
 export class CaseManagement {
+    public checkboxModel = [{
+        name: 'Husband',
+        checked: true,
+        class: 'col-md-2'
+    }, {
+        name: 'Children',
+        checked: true,
+        class: 'col-md-2'
+    }, {
+        name: 'Mother in law',
+        checked: false,
+        class: 'col-md-2'
+    }, {
+        name: 'Father in law',
+        checked: false,
+        class: 'col-md-2'
+    }, {
+        name: 'Brother in law',
+        checked: false,
+        class: 'col-md-2'
+    }, {
+        name: 'Sister in law',
+        checked: false,
+        class: 'col-md-2'
+    }];
 
+    public checkboxPropertiesMapping = {
+        model: 'checked',
+        value: 'name',
+        label: 'name',
+        baCheckboxClass: 'class'
+    };
   constructor() {
   }
 
--- src/MyChoices.Portal/src/app/theme/components/baMsgCenter/baMsgCenter.service.ts
+++ src/MyChoices.Portal/src/app/theme/components/baMsgCenter/baMsgCenter.service.ts
@@ -5,28 +5,28 @@ export class BaMsgCenterService {
 
   private _notifications = [
     {
-      name: 'Vlad',
-      text: 'Vlad posted a new article.',
+      name: 'Ashwini',
+      text: 'Ashwini posted a new article.',
       time: '1 min ago'
     },
     {
-      name: 'Kostya',
-      text: 'Kostya changed his contact information.',
+      name: 'Sareen',
+      text: 'Sareen changed his contact information.',
       time: '2 hrs ago'
     },
     {
       image: 'assets/img/shopping-cart.svg',
-      text: 'New orders received.',
+      text: 'You purchased Quantium t-shirt.',
       time: '5 hrs ago'
     },
     {
-      name: 'Andrey',
-      text: 'Andrey replied to your comment.',
+      name: 'Roger',
+      text: 'Roger replied to your comment.',
       time: '1 day ago'
     },
     {
-      name: 'Nasta',
-      text: 'Today is Nasta\'s birthday.',
+      name: 'Rakesh',
+      text: 'Today is Rakesh\'s birthday.',
       time: '2 days ago'
     },
     {
@@ -35,46 +35,46 @@ export class BaMsgCenterService {
       time: '3 days ago'
     },
     {
-      name: 'Kostya',
-      text: 'Kostya invited you to join the event.',
+      name: 'Samyuktha',
+      text: 'Samyuktha invited you to join the event.',
       time: '1 week ago'
     }
   ];
 
   private _messages = [
     {
-      name: 'Nasta',
-      text: 'After you get up and running, you can place Font Awesome icons just about...',
+          name: 'Rakesh',
+      text: 'Got the my choices demo running',
       time: '1 min ago'
     },
     {
-      name: 'Vlad',
-      text: 'You asked, Font Awesome delivers with 40 shiny new icons in version 4.2.',
+      name: 'Kannan',
+      text: 'What is the status of CheckOut sprint 80',
       time: '2 hrs ago'
     },
     {
-      name: 'Kostya',
-      text: 'Want to request new icons? Here\'s how. Need vectors or want to use on the...',
+      name: 'Roger',
+      text: 'Need to host mychoices website',
       time: '10 hrs ago'
     },
     {
-      name: 'Andrey',
-      text: 'Explore your passions and discover new ones by getting involved. Stretch your...',
+      name: 'Ashwini',
+      text: 'angular 2 demo finished',
       time: '1 day ago'
     },
     {
-      name: 'Nasta',
-      text: 'Get to know who we are - from the inside out. From our history and culture, to the...',
+      name: 'Reddy',
+      text: 'Document db is set up',
       time: '1 day ago'
     },
     {
-      name: 'Kostya',
-      text: 'Need some support to reach your goals? Apply for scholarships across a variety of...',
+      name: 'Kannan',
+      text: 'Need some support to wrap the tech share sessions',
       time: '2 days ago'
     },
     {
-      name: 'Vlad',
-      text: 'Wrap the dropdown\'s trigger and the dropdown menu within .dropdown, or...',
+      name: 'Reddy',
+      text: 'Pricing central KT finished',
       time: '1 week ago'
     }
   ];
